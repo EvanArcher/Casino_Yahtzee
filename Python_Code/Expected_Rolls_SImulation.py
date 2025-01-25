25b515e112a9d75a24a28ab563fde43f4fac3831
Evan Archer <evana@Mac.lan>
2025-01-25 07:37:01 -0700

    started file

---
+++ Python_Code/Expected_Rolls_SImulation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 25 07:34:48 2025
+
+@author: evana
+
+This code is trying to loop through a simulation of yahtzee rolls until completion
+completion is deemed to be a yahtzee or large straight then it will report the results
+"""
+
+#%Libraries
+import numpy as np
+import pandas as pd
+
+
