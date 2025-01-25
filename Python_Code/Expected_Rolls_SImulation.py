160fdb8d121e813650bcc94d2cd104098ebd4ab0
Evan Archer <evana@Mac.lan>
2025-01-25 11:13:47 -0700

    think I added the logic to go for large straights and track it

--- Python_Code/Expected_Rolls_SImulation.py
+++ Python_Code/Expected_Rolls_SImulation.py
@@ -15,7 +15,7 @@ import pandas as pd
 import Yahztee_Function_Library as yfl
 
 #%% Setup variables and winning hands
-num_simulations = 50 #number of games played to completion
+num_simulations = 500 #number of games played to completion
 num_dice = 5  # Number of dice
 num_sides = 6  # Number of sides on a six-sided die
 # straight sets
@@ -49,13 +49,28 @@ for i in range(0,num_simulations):
     # for 4 of a kind and yahtzee then 1-x% of the time go for full house
     # however while rolling a full house, a 4 of a kind and yahtzee are possible
     # so we should record those as well.
+    # Note: ensure we only record each type of roll 1 time during a run
     
     # if set length is 5 we must have at least a small straight
     if len(set(dice_roll)) == 5:
+        df = yfl.dynamic_fill(df,'small_straight', roll_count)
         lg_check = any(set(dice_roll) == lg_set for lg_set in large_straight_sets)
         if lg_check:
             df = yfl.dynamic_fill(df,'large_straight', roll_count)
-            print(set(dice_roll))
+            # print(set(dice_roll))
+        else: 
+            # find which straight we are targeting
+            tar_set, dice_to_keep = yfl.find_closest_set(set(dice_roll), large_straight_sets)
+            while lg_check == False:
+                dice_roll = yfl.reroll(dice_roll, dice_to_keep)
+                roll_count += 1
+                print(dice_roll, roll_count)
+                lg_check = any(set(dice_roll) == lg_set for lg_set in large_straight_sets)
+                tar_set, dice_to_keep = yfl.find_closest_set(set(dice_roll), large_straight_sets)
+            # finally have large straight
+            df = yfl.dynamic_fill(df,'large_straight', roll_count)
+
+
 
 
 
--- Python_Code/Yahztee_Function_Library.py
+++ Python_Code/Yahztee_Function_Library.py
@@ -5,9 +5,16 @@ Created on Sat Jan 25 10:07:19 2025
 
 @author: evana
 """
+#%% Libraries
 import numpy as np
 import pandas as df
 
+#%% Constant values
+num_dice = 5  # Number of dice
+num_sides = 6  # Number of sides on a six-sided die
+
+
+#%%functions
 # Function to fill the first blank spot in a column, or append a new row if no blank spots
 def dynamic_fill(df, column, value):
     # Check if there are any blank (NaN) spots in the column
@@ -23,3 +30,47 @@ def dynamic_fill(df, column, value):
         df = df.append(new_row, ignore_index=True)
     
     return df
+
+
+def find_closest_set(target_set, list_of_sets):
+    """
+    Find the set in the list that has the most numbers in common with the target set.
+    Returns the set, the number of matches, and the matching numbers.
+
+    Args:
+        target_set (set): The incoming set to compare.
+        list_of_sets (list of sets): A list of sets to check against.
+
+    Returns:
+        tuple: The set with the most matches, the number of matches, and the matching numbers.
+    """
+    # Find the set with the largest intersection
+    closest_set = max(list_of_sets, key=lambda s: len(target_set & s))
+    matching_numbers = list(target_set & closest_set)
+    match_count = len(matching_numbers)
+
+    return closest_set, matching_numbers
+
+def reroll(dice_roll, dice_to_keep):
+    """
+    Parameters
+    ----------
+    dice_roll : list/array
+        Current roll.
+    dice_to_keep : list
+        list of dice you want to keep, must put each one you want 
+        i.e. if you have 1,5,5,4,3 and want the 5's put [5,5].
+    Returns
+    -------
+    New dice roll.
+    """
+    # Re-roll dice that are not in the keep values
+    new_roll = []
+    temp_keep_values = dice_to_keep[:]
+    for value in dice_roll:
+        if value in temp_keep_values:
+            new_roll.append(value)  # Keep this die
+            temp_keep_values.remove(value)  # Remove one instance of the value
+        else:
+            new_roll.append(np.random.randint(1, num_sides + 1))  # Re-roll this 
+    return new_roll
